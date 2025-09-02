b9891f9ee4cc8aee3c2dd540495339d1964b52ee
NoobBot-v2 <32234953+NoobBot-v2@users.noreply.github.com>
2025-09-02 11:27:46 +0800

    Update test_LIS.c

--- test_LIS.c
+++ test_LIS.c
@@ -19,14 +19,8 @@ int LIS(int *s, int size, int offset)
     {
         if (last_max < s[i])
         {
-            printf("%-3i\n",last_max);
+            printf("1:%-3i\n",last_max);
             last_max = s[i];
-            length++;
-        }
-        else if (last_max < s[i + 1])
-        {
-            printf("%-3i\n",last_max);
-            last_max = s[i + 1];
             length++;
         }
         i++;
@@ -36,13 +30,31 @@ int LIS(int *s, int size, int offset)
     return (length);
 }
 
+//Find the elements that were skipped in the LIS
+void find_unsorted_subsequence(int *s, int lis_idx, int size)
+{
+    int start = -1;
+    int end = -1;
+    int i = 0;
+    
+    while(i < size - 2)
+    {
+        if (start == -1 && (s[i] > s[i + 1]))
+            start = i;
+        else if (start != -1 && end == -1 && (s[i] < s[i+1]))
+            end = i;
+        i++;
+    }
+    printf("Start: %-3i End: %-3i\n",start,end);
+}
+
 int main()
 {
 
-    int test[20] = {4, -2, 10, 5, -20,
-                    0, 1, 28, -30, 6,
+    int test[20] = { -20, 0, 1, 5, 10,
+                    28, -30, 6,
                     8, -100, -3, 43, 42,
-                    1230, -432, 321, -1, 12};
+                    1230, -432, 321, -1, 12, -2 ,4};
     int size = 20;
     int i = 0;
     int length = 1;
@@ -63,5 +75,6 @@ int main()
     }
     printf("longest_len: %-3i\n",longest_len);
     printf("longest_start_idx: %-3i\n",longest_i);
+    find_unsorted_subsequence(test, longest_i, size);
     return 0;
 }
