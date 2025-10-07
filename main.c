8559dedae4710e8c7187a986f79dad5bf1a6cc2d
Justin Ming Rui Soh <jsoh@c1r6s3.42singapore.sg>
2025-10-07 22:30:24 +0800

    Tested with tester

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jsoh <jsoh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 17:51:00 by jsoh              #+#    #+#             */
-/*   Updated: 2025/10/07 21:48:59 by jsoh             ###   ########.fr       */
+/*   Updated: 2025/10/07 22:29:56 by jsoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,30 +31,39 @@ static void	algo_controller(t_stack *a, t_stack *b, t_arr *lis)
 	rotate_to_top(a, get_min_index(a));
 }
 
+static void	ft_init_ptrs(t_stack **a, t_stack **b, t_arr **lis)
+{
+	*a = NULL;
+	*b = NULL;
+	*lis = NULL;
+}
+
 //./push_swap $(seq -100 100 | shuf | head -n 20)
 int	main(int argc, char const *argv[])
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 	t_arr	*lis;
+	int		status;
 
-	stack_b = NULL;
-	if (argc == 1)
+	ft_init_ptrs(&stack_a, &stack_b, &lis);
+	status = 0;
+	if (argc == 1 || argv[1][0] == '\0')
+	{
+		ft_error_msg();
 		return (0);
+	}
 	if (ft_init(&stack_a, &lis, argc, argv))
+		status = 1;
+	else if (ft_check_dupes(stack_a))
+		status = 1;
+	else
 	{
-		ft_free_all(&stack_a, &stack_b, &lis);
-		ft_error_msg();
-		return (0);
+		stack_create(&stack_b, stack_a->cap, 'b');
+		algo_controller(stack_a, stack_b, lis);
 	}
-	if (ft_check_dupes(stack_a))
-	{
-		ft_free_all(&stack_a, &stack_b, &lis);
-		ft_error_msg();
-		return (0);
-	}
-	stack_create(&stack_b, stack_a -> cap, 'b');
-	algo_controller(stack_a, stack_b, lis);
 	ft_free_all(&stack_a, &stack_b, &lis);
+	if (status)
+		ft_error_msg();
 	return (0);
 }
